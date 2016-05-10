eb664cf7e0a9a96c011b08c25efed1dc0173f29c
damboc <dam.boc@free.fr>
2016-05-10 12:40:13 +0200

    Update install_skin.py

--- scripts/install_skin.py
+++ scripts/install_skin.py
@@ -7,10 +7,10 @@ import socket
 xbmcgui.Dialog().notification('Skin', 'Start skin Update', xbmcgui.NOTIFICATION_INFO, 5000)
 os.system("sudo rm -r /home/pi/skin.CarPC-touch_carbon/");
 xbmcgui.Dialog().notification('Skin', 'Download Update', xbmcgui.NOTIFICATION_INFO, 5000)
-os.system("git clone https://github.com/idorel77/skin.CarPC-touch_carbon");
+os.system("git clone https://github.com/damboc/skin.CarPC-touch_carbon -b 800x480");
 xbmcgui.Dialog().notification('Skin', 'Install Update', xbmcgui.NOTIFICATION_INFO, 5000)
-os.system("sudo rm -r /home/pi/.kodi/addons/skin.CarPC-touch_carbon-master/");
-os.system("sudo mkdir /home/pi/.kodi/addons/skin.CarPC-touch_carbon-master/");
-os.system("sudo rsync -r skin.CarPC-touch_carbon/ /home/pi/.kodi/addons/skin.CarPC-touch_carbon-master/");
+os.system("sudo rm -r /home/pi/.kodi/addons/skin.CarPC-touch_carbon/");
+os.system("sudo mkdir /home/pi/.kodi/addons/skin.CarPC-touch_carbon/");
+os.system("sudo rsync -r skin.CarPC-touch_carbon/ /home/pi/.kodi/addons/skin.CarPC-touch_carbon/");
 xbmcgui.Dialog().notification('Skin', 'Skin Updated', xbmcgui.NOTIFICATION_INFO, 5000)
 xbmc.executebuiltin('ReloadSkin()');
